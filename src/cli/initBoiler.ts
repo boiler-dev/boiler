19305d53fadb765f40bf1c97f65271254668e736
winton <mail@wintoni.us>
2020-02-14 11:50:25 -0800

    Create and update tsconfig files on init

--- src/cli/initBoiler.ts
+++ src/cli/initBoiler.ts
@@ -4,16 +4,68 @@ import {
   ensureFile,
   readFile,
   writeFile,
+  pathExists,
+  readJson,
+  writeJson,
 } from "fs-extra"
 
 export class InitBoiler {
   async run(destDir: string): Promise<void> {
     const boilerDir = join(destDir, "boiler")
 
+    await ensureDir(boilerDir)
+
     await Promise.all([
-      ensureDir(boilerDir),
+      this.createTsConfig(destDir),
       this.updateGitignore(destDir),
     ])
+  }
+
+  async createTsConfig(destDir: string): Promise<void> {
+    const tsConfigPath = join(destDir, "tsconfig.json")
+    const boilerTsConfigPath = join(
+      destDir,
+      "boiler",
+      "tsconfig.cjs.json"
+    )
+
+    const tsConfigExists = await pathExists(tsConfigPath)
+    const boilerTsConfigExists = await pathExists(
+      boilerTsConfigPath
+    )
+
+    if (tsConfigExists) {
+      const tsConfig = await readJson(tsConfigPath)
+
+      if (tsConfig.references) {
+        tsConfig.references.push({
+          path: "./boiler/tsconfig.cjs.json",
+        })
+        await writeJson(tsConfigPath, tsConfig, {
+          spaces: 2,
+        })
+      }
+    }
+
+    if (tsConfigExists && !boilerTsConfigExists) {
+      await writeFile(
+        boilerTsConfigPath,
+        JSON.stringify(
+          {
+            compilerOptions: {
+              composite: true,
+              module: "commonjs",
+              outDir: "../dist/boiler",
+              rootDir: ".",
+              target: "es5",
+            },
+            extends: "../tsconfig-base.json",
+          },
+          null,
+          2
+        )
+      )
+    }
   }
 
   async updateGitignore(destDir: string): Promise<void> {
