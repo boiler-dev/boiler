2e34b1a9d60a0e97f1673a7f1e6893ecfdf55424
winton <mail@wintoni.us>
2020-02-15 20:12:15 -0800

    Add npm install log messages

--- src/npm.ts
+++ src/npm.ts
@@ -8,10 +8,25 @@ export class Npm {
     destDir: string,
     pkgNames: string[]
   ): Promise<SpawnTerminalOutput> {
-    return await spawnTerminal("npm", {
+    const response = await spawnTerminal("npm", {
       args: ["install", "--save-exact", ...pkgNames],
       cwd: destDir,
     })
+
+    const { code, out } = response
+
+    if (code === 0) {
+      // eslint-disable-next-line no-console
+      console.log("✅ Installed npm modules:", pkgNames)
+    } else {
+      console.error(
+        "🚨 Failed to install npm modules:",
+        pkgNames
+      )
+      console.error(out)
+    }
+
+    return response
   }
 }
 
