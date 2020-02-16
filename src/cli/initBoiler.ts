8b2b21eb4871394a0f59ed35f791d2348f06e21a
winton <mail@wintoni.us>
2020-02-15 16:55:46 -0800

    Move tsconfig flow to install instead of init

--- src/cli/initBoiler.ts
+++ src/cli/initBoiler.ts
@@ -1,81 +1,8 @@
 import { join } from "path"
-import {
-  ensureDir,
-  ensureFile,
-  readFile,
-  writeFile,
-  pathExists,
-  readJson,
-  writeJson,
-} from "fs-extra"
+import { ensureFile, readFile, writeFile } from "fs-extra"
 
 export class InitBoiler {
   async run(destDir: string): Promise<void> {
-    const boilerDir = join(destDir, "boiler")
-
-    await ensureDir(boilerDir)
-
-    await Promise.all([
-      this.createTsConfig(destDir),
-      this.updateGitignore(destDir),
-    ])
-  }
-
-  async createTsConfig(destDir: string): Promise<void> {
-    const tsConfigPath = join(destDir, "tsconfig.json")
-    const relBoilerTsConfigPath =
-      "./boiler/tsconfig.cjs.json"
-    const boilerTsConfigPath = join(
-      destDir,
-      relBoilerTsConfigPath
-    )
-
-    const tsConfigExists = await pathExists(tsConfigPath)
-    const boilerTsConfigExists = await pathExists(
-      boilerTsConfigPath
-    )
-
-    if (tsConfigExists) {
-      const tsConfig = await readJson(tsConfigPath)
-
-      if (tsConfig.references) {
-        const found = tsConfig.references.find(
-          ref => ref.path === relBoilerTsConfigPath
-        )
-
-        if (!found) {
-          tsConfig.references.push({
-            path: relBoilerTsConfigPath,
-          })
-          await writeJson(tsConfigPath, tsConfig, {
-            spaces: 2,
-          })
-        }
-      }
-    }
-
-    if (tsConfigExists && !boilerTsConfigExists) {
-      await writeFile(
-        boilerTsConfigPath,
-        JSON.stringify(
-          {
-            compilerOptions: {
-              composite: true,
-              module: "commonjs",
-              outDir: "../dist/boiler",
-              rootDir: ".",
-              target: "es5",
-            },
-            extends: "../tsconfig-base.json",
-          },
-          null,
-          2
-        )
-      )
-    }
-  }
-
-  async updateGitignore(destDir: string): Promise<void> {
     const gitignorePath = join(destDir, ".gitignore")
 
     await ensureFile(gitignorePath)
--- src/cli/installBoiler.ts
+++ src/cli/installBoiler.ts
@@ -1,4 +1,5 @@
 import { join } from "path"
+import { pathExists, readJson, writeJson } from "fs-extra"
 import boiler from "../"
 import boilerFromArg from "../boilerFromArg"
 import addBoiler from "./addBoiler"
@@ -13,6 +14,55 @@ export class InstallBoiler {
 
       const name = await boilerFromArg(repo)
       await boiler.run(name, destDir, setup)
+    }
+  }
+
+  async addTsConfigRef(
+    destDir: string,
+    name: string
+  ): Promise<void> {
+    const tsConfigPath = join(destDir, "tsconfig.json")
+    const relTsConfigPath = `./boiler/${name}/tsconfig.json`
+    const relBoilerTsConfigPath = `./boiler/${name}/tsconfig.boiler.json`
+
+    const tsConfigExists = await pathExists(tsConfigPath)
+    const relTsConfigExists = await pathExists(
+      relTsConfigPath
+    )
+    const relBoilerTsConfigExists = await pathExists(
+      relBoilerTsConfigPath
+    )
+
+    if (!tsConfigExists || !relTsConfigExists) {
+      return
+    }
+
+    if (!relBoilerTsConfigExists) {
+      await writeJson(relBoilerTsConfigPath, {
+        compilerOptions: {
+          composite: true,
+          outDir: "../dist/boiler",
+          target: "es5",
+        },
+        extends: "./tsconfig.json",
+      })
+    }
+
+    const tsConfig = await readJson(tsConfigPath)
+
+    if (tsConfig.references) {
+      const found = tsConfig.references.find(
+        ref => ref.path === relBoilerTsConfigPath
+      )
+
+      if (!found) {
+        tsConfig.references.push({
+          path: relBoilerTsConfigPath,
+        })
+        await writeJson(tsConfigPath, tsConfig, {
+          spaces: 2,
+        })
+      }
     }
   }
 }
