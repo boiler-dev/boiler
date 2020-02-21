479c55aa6f015c43aff3787536dd6cf08335e82b
winton <mail@wintoni.us>
2020-02-20 21:10:42 -0800

    Add more init boilers

--- src/cli/initBoiler.ts
+++ src/cli/initBoiler.ts
@@ -90,7 +90,9 @@ export const teardownBoiler: TeardownBoiler = async ({
       "git@github.com:boiler-dev/package-json-boiler.git",
       "git@github.com:boiler-dev/ts-boiler.git",
       "git@github.com:boiler-dev/eslint-prettier-ts-boiler.git",
-      "git@github.com:boiler-dev/release-boiler.git"
+      "git@github.com:boiler-dev/release-boiler.git",
+      "git@github.com:boiler-dev/mocha-boiler.git",
+      "git@github.com:boiler-dev/vscode-watch-boiler.git"
     )
   }
 }
