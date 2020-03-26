c550b0a02c783816990f4e785ba46b97be4bc204
winton <mail@wintoni.us>
2020-03-26 14:00:55 -0700

    tinyId boiler

---
+++ test/tinyId.spec.ts
@@ -0,0 +1,18 @@
+import expect from "./expect"
+import tinyId from "../src/tinyId"
+
+describe("tinyId", () => {
+  beforeEach(() => tinyId.reset())
+
+  it("generates ids", () => {
+    expect(tinyId.generate()).toBe("a")
+    expect(tinyId.generate()).toBe("b")
+  })
+
+  it("resets", () => {
+    tinyId.reset()
+    expect(tinyId.generate()).toBe("a")
+    tinyId.reset()
+    expect(tinyId.generate()).toBe("a")
+  })
+})
